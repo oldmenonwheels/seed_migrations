8937ebfbb961f370abbfcfcdfb672ad166227e16
Alexey Frolov <alexeyfrolov@Alexeys-MacBook-Pro.local>
2020-07-16 19:33:45 +0300

    initial

---
+++ lib/ecto_seed.ex
@@ -0,0 +1,18 @@
+defmodule EctoSeed do
+  @moduledoc """
+  Documentation for `EctoSeed`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> EctoSeed.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/ecto_seed_test.exs
@@ -0,0 +1,8 @@
+defmodule EctoSeedTest do
+  use ExUnit.Case
+  doctest EctoSeed
+
+  test "greets the world" do
+    assert EctoSeed.hello() == :world
+  end
+end
